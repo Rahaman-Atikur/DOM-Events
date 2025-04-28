c6b8e1dac3e8e1a23bb5e940fae941b2f8e0c20a
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-28 11:40:50 +0100

    EventLister is best approach for event handler

--- js/events.js
+++ js/events.js
@@ -1,1 +1,25 @@
-console.log("Dom events");
+function maleYellow() {
+    document.body.style.backgroundColor = 'yellow';
+}
+function maleRed() {
+    document.body.style.backgroundColor = 'red';
+}
+const btnMakeBlue = document.getElementById('btn-click-blue');
+btnMakeBlue.onclick = function makeBlue() {
+    document.body.style.background = 'blue';
+}
+
+const buttonMakePurple = document.getElementById('btn-make-purple');
+buttonMakePurple.onclick = makePurple;
+function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+document.getElementById('btn-click-green').addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green'
+});
+
+document.getElementById('btn-click-gold').addEventListener('click', function makeGold() {
+    document.body.style.backgroundColor = 'goldenrod';
+})
+

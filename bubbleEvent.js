733db2d546af80d61d4da32e2838058925e20283
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-29 23:26:02 +0100

    Bubble events handler done

---
+++ bubbleEvent.js
@@ -0,0 +1,9 @@
+document.getElementById('item-2')
+    .addEventListener('click', function () {
+        console.log('item 2 clicked');
+    })
+// Ol clicked handler
+document.getElementById('item-list')
+    .addEventListener('click', function () {
+        console.log('Order list Clicked');
+    })
